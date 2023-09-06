a7384123307e5fc1e7ef3a6c5b4678087da3a3f3
SUSSYJONNY <128854842+SUSSYJONNY@users.noreply.github.com>
2023-09-06 17:13:04 -0300

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCVu5584FEJtDWdXrTi1DzS5A5QEAN9noE",
+    authDomain: "cesabefirefox.firebaseapp.com",
+    databaseURL: "https://cesabefirefox-default-rtdb.firebaseio.com",
+    projectId: "cesabefirefox",
+    storageBucket: "cesabefirefox.appspot.com",
+    messagingSenderId: "81244915060",
+    appId: "1:81244915060:web:e071674156274519e39821"
+  };
